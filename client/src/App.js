7d8948428de097ef6eedbcd48bfb2b2a8874fb15
faesalzher <faesalher@gmail.com>
2020-04-01 20:25:13 +0700

    testing heroku deploy -reconfigure Appr.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ const browserHistory = createBrowserHistory();
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://eventmgmt-deploy-testing.herokuapp.com/graphql',
 });
 
 
