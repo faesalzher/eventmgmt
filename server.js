b066e404451716c88d41c64d537850d72d57f7a7
faesalzher <faesalher@gmail.com>
2020-04-01 18:30:48 +0700

    testing heroku deploy 2

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
+// mongo.connect('mongodb://usercpm:usercpm@cluster-shard-00-00-mt0bh.gcp.mongodb.net:27017,cluster-shard-00-01-mt0bh.gcp.mongodb.net:27017,cluster-shard-00-02-mt0bh.gcp.mongodb.net:27017/collaborative-project-management?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
+mongo.connect('mongodb://usereventmgmt:usereventmgmt@cluster0-shard-00-00-wn3yl.mongodb.net:27017,cluster0-shard-00-01-wn3yl.mongodb.net:27017,cluster0-shard-00-02-wn3yl.mongodb.net:27017/eventmgmtdb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
+
+mongo.connection.once('open', () => { console.log('Conneted to MongoDB Cloud\n') })
+
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
@@ -30,8 +35,3 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-// mongo.connect('mongodb://usercpm:usercpm@cluster-shard-00-00-mt0bh.gcp.mongodb.net:27017,cluster-shard-00-01-mt0bh.gcp.mongodb.net:27017,cluster-shard-00-02-mt0bh.gcp.mongodb.net:27017/collaborative-project-management?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
-mongo.connect('mongodb://usereventmgmt:usereventmgmt@cluster0-shard-00-00-wn3yl.mongodb.net:27017,cluster0-shard-00-01-wn3yl.mongodb.net:27017,cluster0-shard-00-02-wn3yl.mongodb.net:27017/eventmgmtdb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
-
-mongo.connection.once('open', () => { console.log('Conneted to MongoDB Cloud\n') })
-
