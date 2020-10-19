e8f1824b82f0d5b90c6287362c072d16ba20c0d9
faesalzher <faesalher@gmail.com>
2020-10-19 19:55:22 +0700

    bugFix

--- client/src/App.js
+++ client/src/App.js
@@ -14,11 +14,10 @@ import { ApolloProvider } from "@apollo/react-hooks";
 const browserHistory = createBrowserHistory();
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "https://eventmgmt-deploy.herokuapp.com/graphql",
 });
 
 export default function App() {
-
 
   return (
     <ApolloProvider client={client}>
